d5fe92afa22cd3b1c08728bab53f4a07ad5f68c8
Jef D <danielsjf@users.noreply.github.com>
2020-11-15 16:45:06 +0100

    Refactor and add solid blocks

--- journey_to_the_centre_of_the_earth/AutoTile.gd
+++ journey_to_the_centre_of_the_earth/AutoTile.gd
@@ -1,27 +1,37 @@
 extends Node2D
 
 export(int) var max_tile_strength = 1 # seconds
+var max_properties = {'strength': max_tile_strength,
+					  'solid': false,
+					 }
 var tile_properties = {}
 
 
-func get_strength(pos):
+func get_prop(prop, pos):
 	if !tile_properties.has(pos):
 		tile_properties[pos] = {}
 		
-	if !tile_properties[pos].has('strength'):
-		tile_properties[pos]['strength'] = max_tile_strength
+	if !tile_properties[pos].has(prop):
+		tile_properties[pos][prop] = max_properties[prop]
+		
+	return tile_properties[pos][prop]
+
+func set_prop(prop, pos, value):
+	get_prop(prop, pos) # initialise
 	
-	print(tile_properties[pos]['strength'])
-	return tile_properties[pos]['strength']
+	tile_properties[pos][prop] = max(0, value)
+	
+func reduce_prop(prop, pos, value):
+	set_prop(prop, pos, get_prop(prop, pos) - value)
+	
+func get_strength(pos):
+	return get_prop('strength', pos)
 
 func set_strength(pos, value):
-	get_strength(pos) # initialise
-	
-	tile_properties[pos]['strength'] = max(0, value)
+	set_prop('strength', pos, value)
 	
 func reduce_strength(pos, value):
-	set_strength(pos, get_strength(pos) - value)
-
+	reduce_prop('strength', pos, value)
 
 func _on_Player_collided(collision, time):
 	var target = collision.collider
@@ -33,10 +43,14 @@ func _on_Player_collided(collision, time):
 		reduce_strength(tile_pos, time)
 		
 		var strength = get_strength(tile_pos)
-		if strength < max_tile_strength:
+		var solid = get_prop('solid', tile_pos)
+		if strength < max_tile_strength and !solid:
 			$Cracks.set_cellv(tile_pos, floor((1-strength/max_tile_strength)*4))
 			
 			if strength == 0:
 				$Cracks.set_cellv(tile_pos, -1)
 				target.set_cellv(tile_pos, -1)
 				target.update_bitmask_area(tile_pos)
+		
+		if solid:
+			pass
