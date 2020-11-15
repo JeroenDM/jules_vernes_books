6df881c7791cc447fe4099cbfa82de14a8da531c
Jef D <danielsjf@users.noreply.github.com>
2020-11-15 12:10:03 +0100

    Fix autotiles

--- journey_to_the_centre_of_the_earth/AutoTile.gd
+++ journey_to_the_centre_of_the_earth/AutoTile.gd
@@ -2,10 +2,13 @@ extends TileMap
 
 
 func _on_Player_collided(collision):
-	if collision.collider is TileMap:
-		var tile_pos = collision.collider.world_to_map(get_node("../Player").position)
-		print(collision.collider.name)
+	var target = collision.collider
+	if target is TileMap:
+		var tile_pos = target.world_to_map(get_node("../Player").position)
+		print(target.name)
 		print(tile_pos)
 		tile_pos -= collision.normal
 		print(tile_pos)
-		collision.collider.set_cellv(tile_pos, -1)
+		target.set_cellv(tile_pos, -1)
+		target.update_bitmask_area(tile_pos)
+		
