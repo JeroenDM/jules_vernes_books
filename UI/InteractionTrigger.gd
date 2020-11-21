6052debe9fcba3c2a9a570790cad4da636eb1582
Jef D <danielsjf@users.noreply.github.com>
2020-11-21 17:24:32 +0100

    More interaction settings

--- UI/InteractionTrigger.gd
+++ UI/InteractionTrigger.gd
@@ -1,13 +1,14 @@
 extends Area2D
 
-export(String) var message = "Replace this text"
+export(String, MULTILINE) var message = "Replace this text"
+export(int) var message_duration = 3
 export(bool) var show_only_once = true
-export(bool) var pause_while_message = true
+export(bool) var pause_while_message = false
 var shown = false
 
 
 func _on_InteractionTrigger_body_entered(body):
 	if !shown:
-		HUDInfo.set_message(message, 3, pause_while_message)
+		HUDInfo.set_message(message, message_duration, pause_while_message)
 		if show_only_once:
 			shown = true
