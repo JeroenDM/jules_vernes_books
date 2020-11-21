7d29cec13f33a615b9aa7d55a93fcf2fce064689
jeroendm <jeroendemaeyer@live.be>
2020-11-21 19:32:31 +0100

    add missing files

---
+++ spacegame/Satellite.gd
@@ -0,0 +1,11 @@
+extends RigidBody2D
+
+var is_colliding := false
+
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+	if not is_colliding:
+		if state.get_contact_count() > 0:
+			PlayerData.health -= 1
+			is_colliding = true
+		else:
+			is_colliding = false
