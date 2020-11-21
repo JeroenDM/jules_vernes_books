eff3b0340c9d1248bfbb3b81d8304f9f0cd4de4c
jeroendm <jeroendemaeyer@live.be>
2020-11-21 21:36:58 +0100

    reset playerdata on retry and fix retry after empty fuel in balloon

--- UI/RetryLevel.gd
+++ UI/RetryLevel.gd
@@ -1,6 +1,8 @@
 extends Button
 
 func _on_pressed() -> void:
+	PlayerData.health = PlayerData.max_health
+	PlayerData.fuel = PlayerData.max_fuel
 	if get_tree().paused:
 		get_tree().paused = false
 	get_tree().reload_current_scene()
