bca7b096caa6d2198d240fde928f444ac26ac6ad
JeroenDM <jeroendemaeyer@live.be>
2020-11-22 14:26:45 +0100

    add messages and implement damage for space game

--- spacegame/Transition.gd
+++ spacegame/Transition.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
 export(String, FILE) var next_scene_path: = ""
+export(String, MULTILINE) var message = ""
 
 func _ready() -> void:
 	$LaunchTimer.start()
 
 
 func _on_LaunchTimer_timeout() -> void:
+	HUDInfo.set_message(message, 3, true)
 	$AnimationPlayer.play("launch")
 
 
