9ade27b06df9cdd493bd825a437aba3682fe9583
JeroenDM <jeroendemaeyer@live.be>
2020-11-22 16:50:50 +0100

    engine particles and other minor fixes and tweaks

--- spacegame/SpaceShip.gd
+++ spacegame/SpaceShip.gd
@@ -4,12 +4,15 @@ export (float) var engine_thrust := 500.0
 export (float) var spin_thrust := 2500.0
 
 onready var audio_player : AudioStreamPlayer = $AudioStreamPlayer
+onready var engine_fire : Particles2D = $rocket/Particles2D
 
 var thrust: = Vector2.ZERO
 var rotation_dir: = 0
 
 func _ready():
 	PlayerData.connect("die", self, "restart")
+	PlayerData.health = PlayerData.max_health
+	PlayerData.fuel = PlayerData.max_fuel
 
 func restart():
 	PlayerData.fuel = PlayerData.max_fuel
@@ -19,11 +22,13 @@ func restart():
 func get_input() -> void:
 	if Input.is_action_pressed("ui_up"):
 		thrust = Vector2(engine_thrust, 0)
+		engine_fire.emitting = true
 		if audio_player.playing == false:
 			audio_player.play()
 	else:
-		audio_player.stop()
 		thrust = Vector2.ZERO
+		engine_fire.emitting = false
+		audio_player.stop()
 	
 	rotation_dir = 0
 	if Input.is_action_pressed("ui_left"):
