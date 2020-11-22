555c65e3e50ff3f5a1e316c6f2983a0b377edfd1
JeroenDM <jeroendemaeyer@live.be>
2020-11-22 20:05:41 +0100

    first play intro music, then the loop

--- autoload/MusicPlayer.gd
+++ autoload/MusicPlayer.gd
@@ -1,6 +1,10 @@
 extends Node
 
+onready var intro : AudioStreamPlayer = $Intro
 onready var background_music : AudioStreamPlayer = $BackgroundMusic
 
 func _ready() -> void:
+	intro.play()
+
+func play_loop():
 	background_music.play()
